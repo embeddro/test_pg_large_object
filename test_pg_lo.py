08af2d6f267a914f33b9ea3cfd4ab8818311f4df
Aleksey Prusakov <abprusakov@sberbank.ru>
2022-03-18 11:59:33 +0300

    add some metrics to test

--- test_pg_lo.py
+++ test_pg_lo.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python
 import os.path
-
 import psycopg2
-from psycopg2.extensions import lobject
 from os import listdir
 from os.path import isdir, isfile, join, getsize
 import time
 import fire
-
 
 
 class PGBenchmark(object):
@@ -120,18 +117,24 @@ class PGBenchmark(object):
             print("Exception: {}".format(e))
         return res
 
-    def get_average_write(self):
+    def get_average_write(self, type_file=''):
         try:
-            self.cur.execute('SELECT AVG(write_to_db_time) FROM test_pg_lo;')
+            if type_file:
+                self.cur.execute(f"SELECT AVG(write_to_db_time) FROM test_pg_lo WHERE file_path LIKE '%{type_file}%' ;")
+            else:
+                self.cur.execute("SELECT AVG(write_to_db_time) FROM test_pg_lo;")
             res = self.cur.fetchone()
             self.conn.commit()
         except (psycopg2.Warning, psycopg2.Error) as e:
             print("Exception: {}".format(e))
         return int(res[0])
 
-    def get_average_read(self):
+    def get_average_read(self, type_file=''):
         try:
-            self.cur.execute('SELECT AVG(read_from_db_time) FROM test_pg_lo;')
+            if type_file:
+                self.cur.execute(f"SELECT AVG(read_from_db_time) FROM test_pg_lo WHERE file_path LIKE '%{type_file}%';")
+            else:
+                self.cur.execute('SELECT AVG(read_from_db_time) FROM test_pg_lo;')
             res = self.cur.fetchone()
             self.conn.commit()
         except (psycopg2.Warning, psycopg2.Error) as e:
@@ -155,10 +158,25 @@ class PGBenchmark(object):
         except (psycopg2.Warning, psycopg2.Error) as e:
             print("Exception: {}".format(e))
 
+    def total_size_lo(self):
+        try:
+            self.cur.execute("SELECT current_setting('block_size');")
+            blk_size = int(self.cur.fetchone()[0])
+            lo_size = int(blk_size / 4)
+            self.cur.execute("SELECT count(*) from pg_catalog.pg_largeobject ;")
+            num_blocks = int(self.cur.fetchone()[0])
+            self.conn.commit()
+        except (psycopg2.Warning, psycopg2.Error) as e:
+            print("Exception: {}".format(e))
+        return int(lo_size * num_blocks /1024 / 1024)
+
+
+
+
 def start(dbname='test_largeobject', user='tester', host='localhost', password='tester', port=5432):
     pgb = PGBenchmark(dbname=dbname, user=user, host=host, password=password, port=port)
-    pgb.clear_all_large_object()
     pgb.clear_file_table()
+    print(f"Заполненность базы (large object): {pgb.total_size_lo()} Mb")
     start_w = int(round(time.time() * 1000))
     pgb.read_from_folder_to_db('./input')
     end_w = int(round(time.time() * 1000))
@@ -166,22 +184,49 @@ def start(dbname='test_largeobject', user='tester', host='localhost', password='
     pgb.write_from_db_to_folder('./output')
     end_r = int(round(time.time() * 1000))
     stat = pgb.get_lowest_write()
+
+    print("-"*30)
+
+    av_small_time = pgb.get_average_read(type_file='small')
+    print(f"Среднее время записи в базу для small файлов: {av_small_time} ms")
+
+    av_medium_time = pgb.get_average_read(type_file='medium')
+    print(f"Среднее время записи в базу для medium файлов: {av_medium_time} ms")
+
+    av_large_time = pgb.get_average_read(type_file='large')
+    print(f"Среднее время записи в базу для large файлов: {av_large_time} ms")
+
+    print("-"*30)
+
+    stat = pgb.get_lowest_write()
     print(f"Самая медленная запись в базу: файл - {stat[0]}, размер - {int(stat[4]/1000)} kB,  время - {stat[2]} ms")
+
     stat = pgb.get_fastest_write()
     print(f"Самая быстрая запись в базу: файл - {stat[0]}, размер - {int(stat[4]/1000)} kB, время - {stat[2]} ms")
+
     stat = pgb.get_average_write()
     print(f"Среднее время записи в базу: {stat} ms")
+
+    print("-"*30)
+
     stat = pgb.get_lowest_read()
     print(f"Самое медленное чтение из базы: файл - {stat[0]}, размер - {int(stat[4]/1000)} kB, время - {stat[3]} ms")
+
     stat = pgb.get_fastest_read()
     print(f"Самое быстрое чтение из базы: файл - {stat[0]}, размер - {int(stat[4]/1000)} kB, время - {stat[3]} ms")
+
     stat = pgb.get_average_read()
     print(f"Среднее время чтения из базы: {stat} ms")
-    print(f"Всего записано в базу: {pgb.get_total_size()} kB за {end_w - start_w} ms")
-    print(f"Чтение из базы и запись на диск всей папки: {pgb.get_total_size()} kB за {end_r - start_r} ms")
-    pgb.clear_all_large_object()
-    pgb.delete_meta_table()
 
+    print("-"*30)
+
+    total_size = pgb.get_total_size()
+    write_time = end_w - start_w
+    read_time = end_r - start_r
+    print(f"Всего записано в базу: {total_size} kB за {write_time} ms, средняя скорость - {int(total_size / write_time * 1000)} kB/c")
+    print(f"Всего прочитано из базы: {total_size} kB за {read_time} ms, средняя скорость - {int(total_size / read_time * 1000)} kB/c")
+#    pgb.clear_all_large_object()
+    pgb.delete_meta_table()
 
 
 if __name__ == '__main__':
